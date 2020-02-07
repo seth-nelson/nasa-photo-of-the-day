388f4131031bdf2e0148a705d7a7b74ad08379d5
Seth Nelson <seth.nelson@outlook.com>
2020-02-06 19:24:26 -0800

    Brad helped me debug my code. It was a syntax error that was keeping me from posting the image from the API

--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LikeButton from "./LikeButton";
+import ImageInfo from "./ImageInfo";
+import Title from "./Title";
+import axios from "axios";
 import "./ApodCard.css";
 
-
 const ApodCard = props => {
-    console.log();
+    const [nasaImg, setNasaImg] = useState('');
+    // console.log();
+
+    useEffect(() => {
+        // console.log(nasaImg);
+
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=inPjaaJyTMnb0cJsR1l50D3EJ8sNVlM6wD7sADgx
+        `)
+            .then(response => {
+                setNasaImg(response.data)
+                console.log(response.data);
+
+            })
+            .catch(error => console.log("oops", error));
+        }, [])
 
     return (
         <div className="photo-post-container">
-
+        <ImageInfo nasaImg={nasaImg} /> 
         </div>
-    )
-};
+    )};
 
-
-export default ApodCard;
+    export default ApodCard;
--- src/components/imageInfo.js
+++ src/components/imageInfo.js
@@ -1,5 +1,6 @@
-import React from "react";
-import "./ApodCard.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ApodCard from "../"
 
 
 const ImageInfo = props => {
@@ -7,12 +8,13 @@ const ImageInfo = props => {
 
     return (
         <div className="img-container">
-            <img src="xxx" alt="xxx" />
+            <h1>{props.nasaImg.title}</h1>
+            <h3>Photo by: {props.nasaImg.copyright}</h3>
+            <img src={props.nasaImg.url} alt={props.nasaImg.title} />
+            <a href={props.nasaImg.hdurl}>View photo in HD</a>
+            <p>{props.nasaImg.explanation}</p>
         </div>
     )
 };
-
-
-
 
 export default ImageInfo;
